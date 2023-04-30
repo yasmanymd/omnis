9b840eef6146c01a0d7743e88536a0c6b5527a5f
yasmanymd <yasmany@gmail.com>
2023-04-29 22:45:53 -0400

    removed old DB

--- db/restore.sh
+++ db/restore.sh
@@ -3,5 +3,5 @@ ls ~/backup
 echo "Folder to restore?"
 read folder 
 docker cp ~/backup/$folder omnis-omnisdb-1:/dump 
-docker exec -i omnis-omnisdb-1 /usr/bin/mongorestore --username root --password secret --authenticationDatabase admin /dump
+docker exec -i omnis-omnisdb-1 /usr/bin/mongorestore --username root --password secret --drop --preserveUUID --authenticationDatabase admin /dump
 docker exec -it omnis-omnisdb-1 rm -rf /dump
