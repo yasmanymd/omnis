c11496bbb65eb2a9191d05245e20a9348440b9db
yasmanymd <yasmany@gmail.com>
2023-05-18 19:49:23 -0400

    fix restore scripts

--- scripts/db/restore.sh
+++ scripts/db/restore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-folder = $1/db
+folder=$1/db
 docker cp $folder omnis-omnisdb-1:/dump 
 docker exec -i omnis-omnisdb-1 /usr/bin/mongorestore --username root --password secret --drop --preserveUUID --authenticationDatabase admin /dump
 docker exec -it omnis-omnisdb-1 rm -rf /dump
--- scripts/keycloack/restore.sh
+++ scripts/keycloack/restore.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-folder = $1/keycloack
+folder=$1/keycloack
 configsFolder=$folder/configs
 dbFolder=$folder/db
 docker cp $configsFolder/standalone.xml omnis-keycloak-1:/opt/jboss/keycloak/standalone/configuration/standalone.xml
 docker cp $configsFolder/standalone-ha.xml omnis-keycloak-1:/opt/jboss/keycloak/standalone/configuration/standalone-ha.xml
+docker exec -it omnis-postgres-1 dropdb -f -U keycloak keycloak
 docker exec -it omnis-postgres-1 createdb -U keycloak keycloak
 docker exec -i omnis-postgres-1 psql -U keycloak keycloak < $dbFolder/keycloak_backup.sql
