5888fcf95c15adbe686941aacf6d1e673f7853b2
yasmanymd <yasmany@gmail.com>
2023-06-06 23:32:05 -0400

    fix candidates auth

--- portal/src/pages/api/candidates.js
+++ portal/src/pages/api/candidates.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next"
-import { authOptions } from "../"
+import { authOptions } from "./auth/[...nextauth]"
 import { getToken } from "next-auth/jwt"
 
 export default async function candidates(req, res) {
