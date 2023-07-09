65ac73e7017a31a58933eb61b0f92aa84c3c0592
yasmanymd <yasmany@gmail.com>
2023-07-08 21:59:24 -0400

    removing sudo

--- scripts/documents/restore.sh
+++ scripts/documents/restore.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 folder=$1/documents
-sudo docker exec omnis-portal-1 rm -rf /usr/src/app/public/docs
-sudo docker exec omnis-portal-1 mkdir -p /usr/src/app/public/docs
-sudo docker cp $folder/. omnis-portal-1:/usr/src/app/public/docs
+docker exec omnis-portal-1 rm -rf /usr/src/app/public/docs
+docker exec omnis-portal-1 mkdir -p /usr/src/app/public/docs
+docker cp $folder/. omnis-portal-1:/usr/src/app/public/docs
--- scripts/keycloack/backup.sh
+++ scripts/keycloack/backup.sh
@@ -4,6 +4,6 @@ configsFolder=$folder/configs
 dbFolder=$folder/db
 mkdir -p $configsFolder
 mkdir -p $dbFolder
-sudo docker cp omnis-keycloak-1:/opt/jboss/keycloak/standalone/configuration/standalone.xml $configsFolder
-sudo docker cp omnis-keycloak-1:/opt/jboss/keycloak/standalone/configuration/standalone-ha.xml $configsFolder
-sudo docker exec -t omnis-postgres-1 pg_dump -U keycloak -d keycloak > $dbFolder/keycloak_backup.sql
+docker cp omnis-keycloak-1:/opt/jboss/keycloak/standalone/configuration/standalone.xml $configsFolder
+docker cp omnis-keycloak-1:/opt/jboss/keycloak/standalone/configuration/standalone-ha.xml $configsFolder
+docker exec -t omnis-postgres-1 pg_dump -U keycloak -d keycloak > $dbFolder/keycloak_backup.sql
